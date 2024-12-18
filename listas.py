16029ec0aef13b2cbb5d29f2a8c85eb47033c8f1
DataSarahBarros <sarahbarros.bi@gmail.com>
2024-12-18 08:35:11 -0300

    criando listas com py

---
+++ listas.py
@@ -0,0 +1,27 @@
+# indice de uma lista sempre começa por 0
+
+lista_numeros = [1,2,3]
+
+lista_numeros[0] = 5
+
+# listas podem ter diferentes dado
+# numeros = [1, 2, 3]
+# strings = ['João', 'Sarh', 'Samuel']
+# decimais = [10.8, 15.2, 33.3]
+
+print(lista_numeros[0])
+print(lista_numeros[1])
+print(lista_numeros[2])
+
+lista_vazia = []
+lista_vazia.append('Sarah')
+lista_vazia.append('Barros')
+print(lista_vazia)
+
+
+# analisando valores (variavel numeros) 
+
+numeros = (10, 9, 8, 7, 6)
+print('total:', len(numeros))
+print('menor valor', min(numeros))
+print('maior valor', max(numeros))
