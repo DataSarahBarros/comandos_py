842fa88b9da1b06db266a35a5b481fd77d5d7d7d
DataSarahBarros <sarahbarros.bi@gmail.com>
2024-12-18 08:35:41 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,18 @@
+#boolean aretoena true or false
+is_python = True
+is_java = False
+
+#boolean armazena condição
+
+ingressos = 50
+compradores = 250
+tem_ingresso_suficiente = (ingressos >= compradores)
+print(tem_ingresso_suficiente)
+
+nome = 'sarah'
+idade = 20
+peso = 55.5
+
+print(nome)
+print(idade)
+print(peso)
