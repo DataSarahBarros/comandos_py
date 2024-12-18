a4d3a6ce29f26a538d6cc4342c82c0cca4e5c741
DataSarahBarros <sarahbarros.bi@gmail.com>
2024-12-18 08:34:44 -0300

    condicionais

---
+++ condicionais.py
@@ -0,0 +1,33 @@
+#1text
+
+valor1 = 10
+valor2 = 50
+
+# if / se
+if valor1 > valor2:
+    print(valor1, 'é maior que', valor2)
+# else / se não
+else:
+    print(valor2, 'é maior que', valor1)
+# elif / ou então
+
+#2test
+
+idade = int( input('informe a sua idade:'))
+
+if idade >= 18:
+    print('entrada liberada')
+else:
+    print('entrada bloqueada')
+
+#3test
+
+salario = float(input("informe o salário:"))
+if salario <= 3000:
+    print('dev junior')
+elif salario > 3000 and salario <= 6000:
+    print('dev pleno')
+elif salario > 6000 and salario <= 15000:
+    print('dev senior')
+else:
+    print('project manager')
