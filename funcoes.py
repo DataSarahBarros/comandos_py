a69049f47eb72721fa5e945b7f606e200048f4ef
DataSarahBarros <sarahbarros.bi@gmail.com>
2024-12-26 16:05:12 -0300

    Create funcoes.py

---
+++ funcoes.py
@@ -0,0 +1,41 @@
+def minha_funcao(valor1, valor2):
+    return valor1 + valor2
+
+resposta = minha_funcao(10,10)
+
+print('resposta', resposta)
+
+#exemplo com repeticoes
+
+def minha_funcao(valor1, valor2):
+    return valor1 + valor2
+
+while True:
+    valor1 = int(input('Valor1:'))
+    valor2 = int(input('Valor2:'))
+    
+    resposta = minha_funcao(valor1, valor2)
+    print(valor1, '+', valor2, '=', resposta)
+
+#exemplo sem repeticoes
+
+valor1 = int(input('Digite um valor:'))
+valor2 = int(input('Digite outro valor:'))
+resposta = valor1 + valor2
+# print('a soma entre', valor1, 'e', valor2, 'vale', resposta)
+print('A soma entre {} e {} vale {}'.format(valor1, valor2, resposta))
+
+
+# ver o tipo (type) do valor que for inserido (input)
+valor = (input('Digite algo:'))
+print(type(valor))
+
+# métodos de teste de tipo
+valor = (input('Digite algo:'))
+print('O tipo primitivo desse valor é', type(valor))
+print('Só tem espacos?', valor.isspace())
+print('É um número?', valor.isnumeric())
+print('É alabétivo?', valor.isalpha())
+print('É alfanumérico?', valor.isalnum())
+print('Está em maiúscula?', valor.isupper())
+print('Está em minúscula?', valor.islower())
