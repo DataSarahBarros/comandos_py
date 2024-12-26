625fb6cd12f5e97ac85aa39b200516d5a11b5139
DataSarahBarros <sarahbarros.bi@gmail.com>
2024-12-26 16:04:29 -0300

    Update repeticoes.py

--- repeticoes.py
+++ repeticoes.py
@@ -1,21 +1,41 @@
-# executar um bloco de instruções repetidamente
 # for = loop sequencial, repetindo ações em cada elemento
-# while = loop que executa ações enquato a condição for verdadeira
 
-for x in range(10):
-    print(x)
-
-while True:
-    print('se eu rodas o script meu pc morre')
-
-# test loop
 nota = []
 
-for x in range(300):
+for x in range(5):
     codigo_aluno = input('RM:')
     nota = float(input('Nota:'))
     resultado = [codigo_aluno, nota]
     nota.append(resultado)
 
-# for x in range (5) 
+print('quantidade de notas', len(nota))
+
 # o x é uma variavel temporaria, a cada repetição ele muda, igual o indice da lista, então a primeira execução o x = 0 e assim por diante. Range(5) = [0,1,2,3,4] = 5 vezes
+
+for n in nota:
+    codigo_aluno =  n[0]
+    nota = n[1]
+    print('O RM', codigo_aluno, 'tirou a nota', nota)
+
+# while = loop que executa ações enquato a condição for verdadeira
+
+while True:
+    print('se eu rodar script meu pc morre')
+
+
+# lista de repeticoes
+
+notas = []
+
+contador = 1
+
+while contador <=5:
+    codigo_aluno = input('RM:')
+    nota = float(input('Notas:'))
+    resultado = (codigo_aluno, nota)
+    notas.append(resultado)
+
+    # alternativa: contador += 1
+    contador = contador + 1
+
+print('quantidade de notas', len(notas))
