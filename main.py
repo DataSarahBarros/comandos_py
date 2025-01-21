30fa900c65803d7ac55de4fe80104fccde5997f9
DataSarahBarros <sarahbarros.bi@gmail.com>
2025-01-21 20:57:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@
 
 # while True:
 #     ultima_fruta = lista_frutas.pop() #método = pop / parâmetro = nenhum: remove e retorna o último item
-#     lista_frutas.insert(0, ultima_fruta)
-
+#     lista_frutas.insert(0, ultima_fruta) #método = insert / parâmetro = item ou posição, ou os dois que é o caso vai colocar o item 'ultima fruta' na posição 0. O métod insert insere um novo intem na posição dada.
 #     print(lista_frutas)
 
 # para interroper o loop, vamos usar a função range, onde f é uma variavel que servira como contador dentro do loop, ela percorre a função range. A função range gera uma sequência de números com indice iguais a lista
@@ -17,23 +16,36 @@
 # segundo exemplo:
 # lista_frutas = ['banana', 'uva', 'kiwi']
 
-# for f in range(1):
+# for f in range(1): 
 #     ultima_fruta = lista_frutas.pop()
 #     lista_frutas.insert(0, ultima_fruta)
 
 #     print(lista_frutas)
 
-# criar uma lista dentro do parametro da função, cinco str na lista. Desses cinco, validar um por um, quais desses é igual a determinada variável, quando printar essa função, trará o indice que esterminada str.
+# criar uma lista dentro do parametro da função, contendo cinco str na lista. Desses cinco, validar um por um, quais desses é igual a determinada variável, quando printar essa função, trará o indice dessa variavel desterminada.
 
-# def indice_fruta (lista_frutas, minhas_frutas): #definir minha função 'indice_frutas' o parametro da função é 'lista_frutas' (a lista a ser pesquisada) e 'minhas_frutas' (a str que quero encontrasr)
+# aprendendo a usar enumerate: é uma função que itera sobre uma sequência (lista) enquanto acompanha o índice de cada elemento
 
-#     indice[] #criando uma lista vazia
+# lista_frutas = ['banana', 'uva', 'kiwi']
 
+# for indice_fruta, fruta in enumerate(lista_frutas):
+#     print('a fruta {} esta no indice {}'.format(fruta, indice_fruta))
 
-     
-#     return indice # retorna o valor da lista onde a função foi chamada
+#resolvendo o problema usando a função enumerate:
 
-# minhas_frutas = lista_frutas() # a função 'lista_frutas' que armazena a lista de frutas na variável 'minhas_frutas'
+def encontrar_indice (indice_frutas, indice_minhas_frutas): #definir minha função 'indice_frutas' o parametro da função é 'lista_frutas' (a lista a ser pesquisada) e 'minhas_frutas' (a str que quero encontrasr)
 
-# indice_fruta = minhas_frutas.index('banana') # método = index / parâmetros = item 'banana' : retorna o número de ocorrencias do item 
-# print('A banana esta no indice: '.format(indice_fruta))
+    for i, str in enumerate(indice_frutas): #Utiliza um loop for com enumerate para percorrer a lista, obtendo o índice (i) e o valor da string (string) em cada iteração.
+        if str == indice_minhas_frutas:
+            return i # retorna o valor da lista onde a função foi chamada
+        return -1 #se o valor da veriavel for diferete de -1
+
+lista_frutas = ['banana', 'maçã', 'laranja', 'uva', 'abacaxi']
+minhas_frutas = 'laranja'
+
+indice = encontrar_indice(lista_frutas, minhas_frutas)
+
+if indice != -1: #o elemento foi encontrado
+  print(f"A fruta '{minhas_frutas}' foi encontrada no indice {indice}.")
+else:
+  print(f"A fruta '{minhas_frutas}' não foi encontrada na lista.")
