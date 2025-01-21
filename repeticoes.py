aff5f466c7f96f14632ff1fdc6d2ee1314fbfb7c
DataSarahBarros <sarahbarros.bi@gmail.com>
2025-01-20 21:16:55 -0300

    Update repeticoes.py

--- repeticoes.py
+++ repeticoes.py
@@ -10,7 +10,10 @@ for x in range(5):
 
 print('quantidade de notas', len(nota))
 
-# o x é uma variavel temporaria, a cada repetição ele muda, igual o indice da lista, então a primeira execução o x = 0 e assim por diante. Range(5) = [0,1,2,3,4] = 5 vezes
+# o x é uma variavel temporaria, a cada repetição ele muda, igual o indice da lista, então a primeira execução o x = 0 e assim por diante. Range(5) é uma função que exucuta cinco vezes = [0,1,2,3,4] = 5 vezes
+
+#faz a lista em notas, a varivel n que percorre a lista notas contem uma lista onde a posição 0 é o código e a posição 1 é a nota
+#ums lista, dentro de uma lista 
 
 for n in nota:
     codigo_aluno =  n[0]
@@ -25,17 +28,17 @@ while True:
 
 # lista de repeticoes
 
-notas = []
+    notas = []
 
-contador = 1
+    contador = 1
 
-while contador <=5:
-    codigo_aluno = input('RM:')
-    nota = float(input('Notas:'))
-    resultado = (codigo_aluno, nota)
-    notas.append(resultado)
+    while contador <=5:
+        codigo_aluno = input('RM:')
+        nota = float(input('Notas:'))
+        resultado = (codigo_aluno, nota)
+        notas.append(resultado)
 
     # alternativa: contador += 1
-    contador = contador + 1
+        contador = contador + 1
 
-print('quantidade de notas', len(notas))
+    print('quantidade de notas', len(notas))
