9ad5db957dc4870dcd48a8f4ade35f7bb6925431
DataSarahBarros <sarahbarros.bi@gmail.com>
2025-04-16 09:19:50 -0300

    Update condicionais.py

--- condicionais.py
+++ condicionais.py
@@ -45,3 +45,31 @@ else:
     resultado = concesionaria(000)
 
     print(resultado)
+
+# def
+
+nota1 = float(input('Digite a primeira nota:'))
+nota2 = float(input('Digite a segunda nota:'))
+media = (nota1 + nota2)/2
+print('A sua média foi {:.1f}'.format(media))
+if media >=6.0:
+    print('Você está acima da média, parabéns!')
+else:
+    print('Você está abaixo da média, estude mais!')
+
+
+#desafio jogo
+
+from random import randint
+from time import sleep
+computador = randint(0,5) #computador vai percorrer essa lista
+print("-=-" * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20)
+jogador = int(input('Em que número eu pensei?'))
+print('processando...')
+sleep(2)
+if jogador == computador:
+    print('Parabéns você conseguiu me vencer')
+else:
+    print('eu pensei no número {} e não no {}'.format(computador, jogador))
